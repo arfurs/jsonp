5a8bcc9fcf2fcf4cf25d520eb1c66538d14f131e
arfurs <arfurs@163.com>
2019-01-22 13:43:40 +0800

    webpack配置

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('Hello, World')
+const fn = () => {
+  console.log('Hello, World')
+}
+
+const p = new Promise(resolve => {
+  resolve('Success')
+})
+
+const asyncFn = async() => {
+  const res = await p()
+  return res
+}
+
+asyncFn().then(res => {
+  console.log(res)
+})
